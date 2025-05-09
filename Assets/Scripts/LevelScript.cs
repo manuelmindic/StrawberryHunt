349f66cbc03f874d8a34b16b58440bffaf811ad7
Timothy Nicholson <91631726+timmy-2003@users.noreply.github.com>
2025-05-09 16:38:41 +0200

    Level Screen almost done

--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,8 +12,17 @@ public class LevelScript : MonoBehaviour
     public Button levelOneButton;
     public Button levelTwoButton;
     public Button levelThreeButton;
+    public TMP_Text levelOneText;
+    public TMP_Text levelTwoText;
+    public TMP_Text levelThreeText;
+    public Sprite disabledSprite;
 
-
+        void Start()
+    {
+        string levelCounter = "1";
+        Debug.Log(levelOneText.text + "Hieiiier");
+        LevelSelector(levelCounter);
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -19,25 +30,48 @@ public class LevelScript : MonoBehaviour
 
     public void LevelSelector(string levelCounter)
     {
-        levelCounter = "1";
+        Debug.Log(levelCounter);
+        Debug.Log(levelOneText.text);
+        Debug.Log(levelCounter == levelOneText.text);
 
-        if (levelCounter == levelOneButton.name)
+
+        if (levelCounter == levelOneText.text)
         {
-            levelOneButton.enabled = true;
-            levelTwoButton.enabled = false;
-            levelThreeButton.enabled = false;
+            Debug.Log("hallo" + levelCounter);
+
+            levelOneButton.interactable = true;
+            if (levelOneButton.interactable)
+            {
+                Debug.Log("Hieiiierrrrrrr" + levelCounter);
+
+            }
+            levelTwoButton.interactable = false;
+            levelTwoButton.image.sprite = disabledSprite;
+            levelThreeButton.interactable = false;
+            levelThreeButton.image.sprite = disabledSprite;
+
         }
-        else if (levelOneButton.name == levelTwoButton.name)
+        else if (levelCounter == levelTwoText.text)
         {
-            levelTwoButton.enabled = true;
-            levelThreeButton.enabled = false;
-            levelOneButton.enabled = false;
+            levelTwoButton.interactable = true;
+            levelThreeButton.image.sprite= disabledSprite;
+            levelThreeButton.interactable = false;
+            levelOneButton.image.sprite = disabledSprite;
+            levelOneButton.interactable = false;
         }
-        else if (levelThreeButton.name == levelOneButton.name)
+        else if (levelCounter == levelThreeText.text)
         {
-            levelOneButton.enabled = false;
-            levelTwoButton.enabled = false;
-            levelThreeButton.enabled = true;
+            levelOneButton.image.sprite = disabledSprite;
+            levelOneButton.interactable = false;
+            levelTwoButton.image.sprite = disabledSprite;
+            levelTwoButton.interactable = false;
+            levelThreeButton.interactable = true;
         }
+    }
+
+    public void pressNextLevel()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     }
