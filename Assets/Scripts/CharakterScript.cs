2105a4e68575714f94fdd47de3c1f26b5b03b645
Manuel Mindic <mmindic@gmx.at>
2025-05-10 09:43:50 +0200

    navigate through scenes

--- Assets/Scripts/CharakterScript.cs
+++ Assets/Scripts/CharakterScript.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CharakterScript : MonoBehaviour
 {
-
-    // Update is called once per frame
-    public Image[] characterImages; // Assign in Inspector
-    public Sprite[] char1Sprites;   // 2 sprites for Character 1
-    public Sprite[] char2Sprites;   // 2 sprites for Character 2
-    public Sprite[] char3Sprites;   // 2 sprites for Character 3
+    public Image[] characterImages;
+    public Sprite[] char1Sprites;
+    public Sprite[] char2Sprites;
+    public Sprite[] char3Sprites;
 
     private float timer;
     private bool showAltImage = false;
@@ -39,5 +38,15 @@ public class CharakterScript : MonoBehaviour
             characterImages[1].sprite = showAltImage ? char2Sprites[1] : char2Sprites[0];
             characterImages[2].sprite = showAltImage ? char3Sprites[1] : char3Sprites[0];
         }
+    }
+
+    public void ContinueScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void BackScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
