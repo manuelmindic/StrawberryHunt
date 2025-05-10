ba1735217001f88035491fdbd76736dd37c98a29
annapoglitsch <anna.poglitsch@outlook.de>
2025-05-10 17:06:47 +0200

    small changes

--- Assets/Scripts/StartScript.cs
+++ Assets/Scripts/StartScript.cs
@@ -2,21 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartScript : MonoBehaviour
 {
     public AudioSource levelClickSound;
     public AudioClip clickClip;
-    // Start is called before the first frame update
+    public Image image1;
+    public Image image2;
+    public Sprite sprite1;
+    public Sprite sprite2;
+
+    public Image[] characterImages;
+    public Sprite[] char1Sprites;
+    public Sprite[] char2Sprites;
+    public Sprite[] char3Sprites;
+    public Sprite[] char4Sprites;
+
+    private float timer;
+    private bool showAltImage = false;
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer >= 0.75f)
+        {
+            timer = 0f;
+            showAltImage = !showAltImage;
+
+            characterImages[0].sprite = showAltImage ? char1Sprites[1] : char1Sprites[0];
+            characterImages[1].sprite = showAltImage ? char2Sprites[1] : char2Sprites[0];
+            characterImages[2].sprite = showAltImage ? char3Sprites[1] : char3Sprites[0];
+            characterImages[3].sprite = showAltImage ? char4Sprites[1] : char4Sprites[0];
+        }
     }
 
     private IEnumerator PlaySoundAndLoad(string sceneName = "")
@@ -25,17 +48,17 @@ public class StartScript : MonoBehaviour
         {
             Debug.Log("Playing sound...");
             levelClickSound.PlayOneShot(clickClip);
-            yield return new WaitForSeconds(clickClip.length); 
+            yield return new WaitForSeconds(clickClip.length);
         }
 
-        
+
         if (!string.IsNullOrEmpty(sceneName))
         {
             SceneManager.LoadScene(sceneName);
         }
         else
         {
-            
+
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
@@ -47,12 +70,12 @@ public class StartScript : MonoBehaviour
 
     public void CreditScene()
     {
-        StartCoroutine(PlaySoundAndLoad("CreditScene")); 
+        StartCoroutine(PlaySoundAndLoad("CreditScene"));
     }
 
     public void TutorialScene()
     {
-        StartCoroutine(PlaySoundAndLoad("TutorialScene")); 
+        StartCoroutine(PlaySoundAndLoad("TutorialScene"));
     }
 
     public void QuitGame()
@@ -60,3 +83,5 @@ public class StartScript : MonoBehaviour
         Application.Quit();
     }
 }
+
+   
