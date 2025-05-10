9fd2f54e1ce46cce16a06e2c900f8ec25f27032d
annapoglitsch <anna.poglitsch@outlook.de>
2025-05-10 12:10:08 +0200

    Updated Logic

--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -18,24 +18,18 @@ public class LevelScript : MonoBehaviour
     public Sprite disabledSprite;
 
 
-    public Button LevelButton;
-
-    public float speed = 2f;
-    public float height = 8f;
-    public AudioSource levelClickSound;
-    public Vector3 originPos;
 
     void Start()
     {
         string levelCounter = "2";
         LevelSelector(levelCounter);
 
-        originPos = transform.localPosition;
+        
     }
 
     private void Update()
     {
-         upAndDownLevel();    
+        
     }
 
     public void LevelSelector(string levelCounter)
@@ -77,25 +71,8 @@ public class LevelScript : MonoBehaviour
         }
     }
 
-    public void pressNextLevel()
-    {
-        if (LevelButton.interactable == true)
-        {
-            Debug.Log("audiooo");
-            levelClickSound.Play();
-           // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-    }
-    public void upAndDownLevel()
-    {
-        if (LevelButton.interactable == true)
-        {
-            
-
-            float newY = Mathf.Sin(Time.time * speed) * height;
-            transform.localPosition = originPos + new Vector3(0, newY, 0);
-        }
-    }
+    
+   
 
 
 }
